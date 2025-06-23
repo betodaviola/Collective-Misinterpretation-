f23ec23856f560450050db08510476ca8f940068
betodaviola <betodaviola@gmail.com>
2025-06-23 14:18:09 -0500

    docker-compose for php server working | worked on the audience form and admin control webpages

--- webform/input_handler.php
+++ webform/input_handler.php
@@ -1,5 +1,13 @@
 <?php
-  $mov_description = htmlspecialchars($_POST["mov_description"]);
+  $mov_description = $_POST["mov_description"]?? '';//question marks and quotation at the end means to return an empty string if nothing is there to submit
 
-  echo  $mov_description;
+if (!$mov_description) {
+    echo "No review received.";
+    exit;
+}
+
+// Append to descriptions file
+$input = "<input>\n" . htmlentities($mov_description) . "\n</input>\n";
+file_put_contents(__DIR__ . '/input-tests/audience_input.txt', $input, FILE_APPEND);
+
 ?>
